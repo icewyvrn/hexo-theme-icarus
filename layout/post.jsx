f1a21a54022a96a5e1b264835ddd01f3bc8eec12
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-19 20:27:40 +0800

    Update post.jsx

--- layout/post.jsx
+++ layout/post.jsx
@@ -5,7 +5,7 @@ module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
 
-        return <div><div class="card widget" data-type="adsense"><div class="card-content" style="padding: 0.5rem;"><div class="menu"><h3 class="menu-label">Advertisement</h3><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9870073903926891"
+        return <div><div class="card widget" data-type="adsense"><div class="card-content" style="padding: 0.5rem 0rem;"><div class="menu"><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9870073903926891"
      crossorigin="anonymous"></script>
 <ins class="adsbygoogle"
      style="display:block"
