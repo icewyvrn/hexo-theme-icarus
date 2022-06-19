92a37c08fbea6f4a35cea67a7fa81414d832c471
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-19 19:18:57 +0800

    adsense top widget

--- layout/post.jsx
+++ layout/post.jsx
@@ -5,6 +5,6 @@ module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
 
-        return <Article config={config} page={page} helper={helper} index={false} />;
+        return <div><div class="card widget" data-type="adsense"><div class="card-content"><div class="menu"><h3 class="menu-label">Advertisement</h3><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9870073903926891" data-ad-slot="5069768705" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle=window.adsbygoogle||[]).push({})</script></div></div></div><Article config={config} page={page} helper={helper} index={false} /></div>;
     }
 };
