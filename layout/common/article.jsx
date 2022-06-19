6e68c6a4282e02811fa911a819c5293eeb33e1fb
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-19 21:14:46 +0800

    Update article.jsx

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -115,7 +115,7 @@ module.exports = class extends Component {
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-9870073903926891"
-     data-ad-slot="4222471330"
+     data-ad-slot="5058160418"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
 <script>
