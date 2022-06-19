0690cf6b70891f6d875044561cca21f402d05c7e
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-19 19:43:51 +0800

    Update post.jsx

--- layout/post.jsx
+++ layout/post.jsx
@@ -8,9 +8,11 @@ module.exports = class extends Component {
         return <div><div class="card widget" data-type="adsense"><div class="card-content"><div class="menu"><h3 class="menu-label">Advertisement</h3><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9870073903926891"
      crossorigin="anonymous"></script>
 <ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
+     style="display:block"
      data-ad-client="ca-pub-9870073903926891"
-     data-ad-slot="5069768705"></ins>
+     data-ad-slot="4222471330"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script></div></div></div><Article config={config} page={page} helper={helper} index={false} /></div>;
