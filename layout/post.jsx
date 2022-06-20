732beb2b3a0235ae1828fdb3124943c11874c22d
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-21 07:48:17 +0800

    adsense below title

--- layout/post.jsx
+++ layout/post.jsx
@@ -5,16 +5,6 @@ module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
 
-        return <div><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9870073903926891"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-9870073903926891"
-     data-ad-slot="9664771212"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script><Article config={config} page={page} helper={helper} index={false} /></div>;
+        return <Article config={config} page={page} helper={helper} index={false} />;
     }
 };
