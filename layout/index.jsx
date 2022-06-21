acf0f6607b9e8a05f6a77b19e850dcf8ba7d0390
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-21 12:38:37 +0800

    Update index.jsx

--- layout/index.jsx
+++ layout/index.jsx
@@ -8,6 +8,16 @@ module.exports = class extends Component {
         const { __, url_for } = helper;
 
         return <Fragment>
+            <div style="margin-bottom: 1rem;"><ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-9870073903926891"
+         data-ad-slot="2892313450"
+         data-ad-format="horizontal"
+         data-full-width-responsive="true"></ins>
+    <script
+                  dangerouslySetInnerHTML={{
+                    __html: '(adsbygoogle = window.adsbygoogle || []).push({});',
+                  }}></script></div>
             {page.posts.map(post => <Article config={config} page={post} helper={helper} index={true} />)}
             {page.total > 1 ? <Paginator
                 current={page.current}
