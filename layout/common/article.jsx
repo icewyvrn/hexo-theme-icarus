79c60ddeeaa962e8b5f908c4ee533af1e68ec8fd
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-21 09:15:46 +0800

    adsense

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -92,17 +92,6 @@ module.exports = class extends Component {
                     {page.title !== '' ? <h1 class="title is-3 is-size-4-mobile" style="font-family: Helvetica, -apple-system, BlinkMacSystemFont, Roboto, 'Liberation Sans', Arial, sans-serif; font-weight: bold;">
                         {index ? <a class="link-muted" href={url_for(page.link || page.path)}>{page.title}</a> : page.title}
                     </h1> : null}
-                    {!index ? <div>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-9870073903926891"
-     data-ad-slot="2788145755"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script
-              dangerouslySetInnerHTML={{
-                __html: '(adsbygoogle = window.adsbygoogle || []).push({});',
-              }}></script></div> : null}
                     {/* Content/Excerpt */}
                     <div class="content" dangerouslySetInnerHTML={{ __html: index && page.excerpt ? page.excerpt : page.content }}></div>
                     {/* Licensing block */}
--- layout/post.jsx
+++ layout/post.jsx
@@ -5,6 +5,15 @@ module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
 
-        return <Article config={config} page={page} helper={helper} index={false} />;
+        return <><div><ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-9870073903926891"
+         data-ad-slot="2788145755"
+         data-ad-format="horizontal"
+         data-full-width-responsive="true"></ins>
+    <script
+                  dangerouslySetInnerHTML={{
+                    __html: '(adsbygoogle = window.adsbygoogle || []).push({});',
+                  }}></script></div><Article config={config} page={page} helper={helper} index={false} /></>;
     }
 };
