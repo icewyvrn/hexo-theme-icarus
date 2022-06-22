5aa38d4e59e6804351d1284ba6fd22b5bcd11234
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-22 20:24:11 +0800

    adsense

--- layout/post.jsx
+++ layout/post.jsx
@@ -9,7 +9,8 @@ module.exports = class extends Component {
          style="display:block"
          data-ad-client="ca-pub-9870073903926891"
          data-ad-slot="2892313450"
-         data-ad-format="horizontal"></ins>
+         data-ad-format="horizontal"
+         data-full-width-responsive="false"></ins>
     <script
                   dangerouslySetInnerHTML={{
                     __html: '(adsbygoogle = window.adsbygoogle || []).push({});',
