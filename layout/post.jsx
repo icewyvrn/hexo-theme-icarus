f5a8d704da1c1693c0d28f773fd7553976835f83
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-06-22 20:11:51 +0800

    Update post.jsx

--- layout/post.jsx
+++ layout/post.jsx
@@ -5,6 +5,14 @@ module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
 
-        return <Article config={config} page={page} helper={helper} index={false} />;
+        return <><div style="margin-bottom: 1rem;"><ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-9870073903926891"
+         data-ad-slot="2892313450"
+         data-ad-format="horizontal"</ins>
+    <script
+                  dangerouslySetInnerHTML={{
+                    __html: '(adsbygoogle = window.adsbygoogle || []).push({});',
+                  }}></script></div><Article config={config} page={page} helper={helper} index={false} /></>;
     }
 };
