d2c26e9670743f14259fc6cd349a07ecf7110cc4
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-08-14 16:49:18 +0800

    styling

--- layout/index.jsx
+++ layout/index.jsx
@@ -8,16 +8,6 @@ module.exports = class extends Component {
         const { __, url_for } = helper;
 
         return <Fragment>
-            <div><ins class="adsbygoogle"
-                style="display:block; margin-top: 0.5rem;"
-                data-ad-client="ca-pub-9870073903926891"
-                data-ad-slot="2892313450"
-                data-ad-format="horizontal"
-                data-full-width-responsive="false"></ins>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: '(adsbygoogle = window.adsbygoogle || []).push({});',
-                    }}></script></div>
             {page.posts.map(post => <Article config={config} page={post} helper={helper} index={true} />)}
             {page.total > 1 ? <Paginator
                 current={page.current}
