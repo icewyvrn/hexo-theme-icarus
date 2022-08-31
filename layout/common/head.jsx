cd1136fa5e80010320253afad5077799a6dc4d77
icewyvrn <41411360+icewyvrn@users.noreply.github.com>
2022-08-31 14:37:12 +0800

    Update head.jsx

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -138,9 +138,9 @@ module.exports = class extends Component {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
-        <link as="font" crossorigin="anonymous" href="/fonts/ubuntu-v20-latin-regular.woff2" rel="preload" />
-        <link as="font" crossorigin="anonymous" href="/fonts/ubuntu-v20-latin-500.woff2" rel="preload" />
-        <link as="font" crossorigin="anonymous" href="/fonts/ubuntu-v20-latin-700.woff2" rel="preload" />
+        <link as="font" crossorigin="anonymous" href="https://d33wubrfki0l68.cloudfront.net/8d0b1718aacad934fd0043c87cbc54aa091396bf/6485b/fonts/ubuntu-v20-latin-regular.woff2" rel="preload" />
+        <link as="font" crossorigin="anonymous" href="https://d33wubrfki0l68.cloudfront.net/0e9f9333078e5df9245630ff6f68ba1d9da3c403/0a3e5/fonts/ubuntu-v20-latin-500.woff2" rel="preload" />
+        <link as="font" crossorigin="anonymous" href="https://d33wubrfki0l68.cloudfront.net/fb4ed2e52e2a8d7ac50a7618a0c2ea5507a24ef3/f269c/fonts/ubuntu-v20-latin-700.woff2" rel="preload" />
         {noIndex ? <meta name="robots" content="noindex" /> : null}
         {meta && meta.length ? <MetaTags meta={meta} /> : null}
 
